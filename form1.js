f39dfb4c4d8ea3177fdeeabb70a87d4a36bf64ed
folatheman <folajunior1@gmail.com>
2022-08-31 16:04:10 -0400

    init

--- form1.js
+++
@@ -1,7 +0,0 @@
-function validateform(){
-    var password=document.getElementById('password').value;
-
-    if (password.length<=6){
-        alert('password must be longer than 6 characters')
-    }
-}
